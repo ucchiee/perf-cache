ec62139d72bbfe75a69e9cab1e94d740885348fd
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-04-20 22:06:24 +0900

    restore-idx-from-cache.c: deleted tries

--- restore-idx-from-cache.c
+++ restore-idx-from-cache.c
@@ -44,7 +44,7 @@ void touch_array2(size_t x) {
 
 void readMemoryByte(int cache_hit_threshold, size_t secret_x, uint8_t value[2], int score[2]) {
   static int results[256];
-  int tries, i, j, k, mix_i;
+  int i, j, k, mix_i;
   unsigned int junk = 0;
   size_t training_x, x;
   register uint64_t time1, time2;
@@ -52,49 +52,46 @@ void readMemoryByte(int cache_hit_threshold, size_t secret_x, uint8_t value[2], 
 
   for (i = 0; i < 256; i++)
     results[i] = 0;
-  for (tries = 999; tries > 0; tries--) {
 
-    /* Flush array2[512*(0..255)] from cache */
-    for (i = 0; i < 256; i++)
-      _mm_clflush( & array2[i * 512]); /* intrinsic for clflush instruction */
+  /* Flush array2[512*(0..255)] from cache */
+  for (i = 0; i < 256; i++)
+    _mm_clflush( & array2[i * 512]); /* intrinsic for clflush instruction */
 
-    touch_array2(secret_x);
+  touch_array2(secret_x);
 
-    /* Time reads. Order is lightly mixed up to prevent stride prediction */
-    for (i = 0; i < 256; i++) {
-      mix_i = ((i * 167) + 13) & 255;
-      addr = & array2[mix_i * 512];
+  /* Time reads. Order is lightly mixed up to prevent stride prediction */
+  for (i = 0; i < 256; i++) {
+    mix_i = ((i * 167) + 13) & 255;
+    addr = & array2[mix_i * 512];
 
-    /*
-    We need to accuratly measure the memory access to the current index of the
-    array so we can determine which index was cached by the malicious mispredicted code.
+  /*
+  We need to accuratly measure the memory access to the current index of the
+  array so we can determine which index was cached by the malicious mispredicted code.
 
-    The best way to do this is to use the rdtscp instruction, which measures current
-    processor ticks, and is also serialized.
-    */
+  The best way to do this is to use the rdtscp instruction, which measures current
+  processor ticks, and is also serialized.
+  */
 
-      // restore secret_x from cache access
-      time1 = __rdtscp( & junk); /* READ TIMER */
-      junk = * addr; /* MEMORY ACCESS TO TIME */
-      time2 = __rdtscp( & junk) - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
+    // restore secret_x from cache access
+    time1 = __rdtscp( & junk); /* READ TIMER */
+    junk = * addr; /* MEMORY ACCESS TO TIME */
+    time2 = __rdtscp( & junk) - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
 
-      if ((int)time2 <= cache_hit_threshold)
-        results[mix_i]++; /* cache hit - add +1 to score for this value */
-    }
-
-    /* Locate highest & second-highest results results tallies in j/k */
-    j = k = -1;
-    for (i = 0; i < 256; i++) {
-      if (j < 0 || results[i] >= results[j]) {
-        k = j;
-        j = i;
-      } else if (k < 0 || results[i] >= results[k]) {
-        k = i;
-      }
-    }
-    if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
-      break; /* Clear success if best is > 2*runner-up + 5 or 2/0) */
+    if ((int)time2 <= cache_hit_threshold)
+      results[mix_i]++; /* cache hit - add +1 to score for this value */
   }
+
+  /* Locate highest & second-highest results results tallies in j/k */
+  j = k = -1;
+  for (i = 0; i < 256; i++) {
+    if (j < 0 || results[i] >= results[j]) {
+      k = j;
+      j = i;
+    } else if (k < 0 || results[i] >= results[k]) {
+      k = i;
+    }
+  }
+
   results[0] ^= junk; /* use junk so code above won’t get optimized out*/
   value[0] = (uint8_t) j;
   score[0] = results[j];
