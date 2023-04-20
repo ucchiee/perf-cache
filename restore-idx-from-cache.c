004be685df26c998912bfc30ebf3f861c649c9fc
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-04-21 00:27:16 +0900

    restore-idx-from-cache.c: implimented poc of flush and reload

--- restore-idx-from-cache.c
+++ restore-idx-from-cache.c
@@ -38,11 +38,11 @@ void touch_array2(size_t x) {
   //   temp &= array2[array1[x] * 512];
   // }
   
-  // If guard is discarded.
+  // discarded if guard
   temp &= array2[x * 512];
 }
 
-void readMemoryByte(int cache_hit_threshold, size_t secret_x, uint8_t value[2], int score[2]) {
+void flush_reload(int cache_hit_threshold, size_t secret_x, uint8_t value[2], int score[2], uint64_t counter[256]) {
   static int results[256];
   int i, j, k, mix_i;
   unsigned int junk = 0;
@@ -60,6 +60,8 @@ void readMemoryByte(int cache_hit_threshold, size_t secret_x, uint8_t value[2], 
   touch_array2(secret_x);
 
   /* Time reads. Order is lightly mixed up to prevent stride prediction */
+  // CPU に先読みされないように読む順番を工夫しているが、
+  // これでも256に近い値は先読みされてしまうっぽい。
   for (i = 0; i < 256; i++) {
     mix_i = ((i * 167) + 13) & 255;
     addr = & array2[mix_i * 512];
@@ -72,13 +74,17 @@ void readMemoryByte(int cache_hit_threshold, size_t secret_x, uint8_t value[2], 
   processor ticks, and is also serialized.
   */
 
-    // restore secret_x from cache access
+    // メモリアクセスにかかる時間から secret_x を復元する。
+    // touch_array2() でアクセスしたアドレスは高速に読めるが、
+    // それ以外のアドレスはキャッシュされていないため読むのに時間がかかる。
     time1 = __rdtscp( & junk); /* READ TIMER */
     junk = * addr; /* MEMORY ACCESS TO TIME */
     time2 = __rdtscp( & junk) - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
 
     if ((int)time2 <= cache_hit_threshold)
       results[mix_i]++; /* cache hit - add +1 to score for this value */
+
+    counter[mix_i] = time2;
   }
 
   /* Locate highest & second-highest results results tallies in j/k */
@@ -99,34 +105,38 @@ void readMemoryByte(int cache_hit_threshold, size_t secret_x, uint8_t value[2], 
   score[1] = results[k];
 }
 
-int main(void) {
+int main(int argc, char** argv) {
 
   int score[2];
   uint8_t value[2];
-  // size_t malicious_x = (size_t)(secret - (char *)array1);
-  size_t malicious_x = 32;
-  int len = 40;
+  uint64_t counter[256];
+  size_t idx = 32;
 
   // !! IMPORTANT !!
   for (int i = 0; i < (int)sizeof(array2); i++) {
     array2[i] = 1; /* write to array2 so in RAM not copy-on-write zero pages */
   }
 
-  while (--len >= 0) {
-    printf("Reading at malicious_x = %p... ", (void * ) malicious_x);
-
-    readMemoryByte(80, malicious_x++, value, score);
-
-    // print result
-    printf("%s: ", (score[0] >= 2 * score[1] ? "Success" : "Unclear"));
-    printf("0x%02X=’%c’ score=%d ", value[0],
-        (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
-    if (score[1] > 0) {
-      printf("(second best: 0x%02X=’%c’ score=%d)", value[1],
-          (value[1] > 31 && value[1] < 127 ? value[1] : '?'), score[1]);
-    }
-
-    printf("\n");
+  if (argc >= 2) {
+    sscanf(argv[1], "%ld", &idx);
   }
+
+  // array2[idx * 512] にアクセスし、
+  // キャッシュから idx の値を復元・推測する
+  flush_reload(80, idx, value, score, counter);
+
+  // 結果の表示
+  for (int i = 0; i < 256; i++) {
+    printf("array[%3.d * 512]: %ld\n", i, counter[i]);
+  }
+  printf("\n");
+
+  printf("accessed array[%zu * 512]\n\n", idx);
+  printf("Access Time:\n");
+  printf("Best   (array[__%3.hhu__ * 512]) : %zu\n", value[0], counter[value[0]]);
+  if (score[1] > 0)
+    printf("Second (array[__%3.hhu__ * 512]) : %zu\n", value[1], counter[value[0]]);
+  printf("\n");
+
   return 0;
 }
